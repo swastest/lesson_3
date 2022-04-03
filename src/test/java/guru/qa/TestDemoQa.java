3025b475be92b1503bc56b6c914326e35d8ce6af
Tatiana Kazakova <test.kazakova@gmail.com>
2022-04-03 10:59:02 +0300

    added resurses

--- src/test/java/guru/qa/TestDemoQa.java
+++ src/test/java/guru/qa/TestDemoQa.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestDemoQa {
@@ -25,12 +26,17 @@ public class TestDemoQa {
         $("#userEmail").setValue("romashka@mail.ru");
         $("#userNumber").setValue("8800200060");
         $("#dateOfBirthInput").click();
-        // $("#dateOfBirthInput").selectOptionByValue("04 May 1991");  //WRONG
         $(".react-datepicker__month-select").click();
         $(".react-datepicker__month-select").selectOptionByValue("4");
         $(".react-datepicker__year-select").selectOptionByValue("1991");
         $x("//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div[7]").click();
-       // $("#genterWrapper").selectRadio("Female"); wrong
+        $("#subjectsInput").setValue("Hindi").pressEnter();
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#currentAddress").setValue("Eto to4no Java?");
+        $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
+        $("#react-select-4-input").setValue("Merrut").pressEnter();
+
 
 
 
