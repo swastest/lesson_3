d3150153320d1f35faeb57ea6c14f6b7243286a4
Tatiana Kazakova <test.kazakova@gmail.com>
2022-04-03 19:52:14 +0300

    added pravki xpath del

--- src/test/java/guru/qa/TestDemoQa.java
+++ src/test/java/guru/qa/TestDemoQa.java
@@ -30,7 +30,7 @@ public class TestDemoQa {
         //$(".react-datepicker__month-select").click();
         $(".react-datepicker__month-select").selectOptionByValue("4");
         $(".react-datepicker__year-select").selectOptionByValue("1991");
-        $x("//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div[7]").click();
+        $(".react-datepicker__day--004").click();
         $("#subjectsInput").setValue("Hindi").pressEnter();
         $("#genterWrapper").$(byText("Male")).click();
         $("#hobbiesWrapper").$(byText("Reading")).click();
